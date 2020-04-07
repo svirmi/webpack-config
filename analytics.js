3c3a1593bbee9ad8622e011bb1f428939ab5bd85
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-04-07 09:01:34 +0300

    analytics dummy script ready

--- analytics.js
+++ analytics.js
@@ -1,4 +1,21 @@
 function createAnalytics() {
-    let counter = 0
+    let counter = 0;
+    let isDestroyed = false;
 
+    const listener = () => counter++;
+
+    document.addEventListener('click', listener);
+
+    return {
+        destroy() {
+            document.removeEventListener('click', listener);
+            isDestroyed = true;
+        },
+
+        getClicks() {
+            return isDestroyed ? counter : 'Analytics destroyed';
+        }
+    }
 }
+
+window.analytics = createAnalytics();
