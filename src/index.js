cba50164e51790982bf777fd4b6422adcdf4a63c
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-04-21 09:24:19 +0300

    loading json from file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './styles/styles.css';
+import json from './assets/json.json';
 import Post from "./Post";
 
 const post = new Post('Title goes here');
 
 console.log('Post to String: ', post.toString());
+
+console.log('json: ', json);
