6e094a1bc61eb23bb9b8caa8f0d86ca657f44080
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-04-22 08:36:29 +0300

    handling images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './styles/styles.css';
 import json from './assets/json.json';
+import logo from './assets/logo-on-white-bg.png';
 import Post from "./Post";
 
-const post = new Post('Title goes here');
+const post = new Post('Title goes here', logo);
 
 console.log('Post to String: ', post.toString());
 
