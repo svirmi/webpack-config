43bba62a6c411a3edea6e70889a16e8472ed9794
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-04-23 09:06:35 +0300

    using alias for paths

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './styles/styles.css';
-import json from './assets/json';
+import '@/styles/styles.css';
+import json from '@/assets/json';
 import csv from './assets/research-and-development-survey-2019-csv.csv';
-import logo from './assets/logo-on-white-bg.png';
+import logo from '@/assets/logo-on-white-bg.png';
 import Post from "@models/Post";
 
 const post = new Post('Title goes here', logo);
