1953494f8eb99d1a0d93d260dee9569025d38ef9
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-04-28 09:03:48 +0300

    terser-webpack-plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
