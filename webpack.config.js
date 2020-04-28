663d187bfbd3568c553d53c85c9f68345cf9e75f
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-04-28 09:15:02 +0300

    DEV/PROD scripts optimization

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,28 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === "development";
 const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: "all"
+        }
+    };
+
+    if(isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ];
+    }
+
+    return config;
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -18,11 +37,7 @@ module.exports = {
         filename: "[name].[hash].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
-    optimization: {
-        splitChunks: {
-            chunks: "all"
-        }
-    },
+    optimization: optimization(),
     devServer: {
         port: 4200,
         hot: isDev
