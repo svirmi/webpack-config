649cd8fcb06b356bc1f60a1b548427893df441a8
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-04-30 08:48:49 +0300

    babel preset added

--- webpack.config.js
+++ webpack.config.js
@@ -106,7 +106,19 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                loader: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    "useBuiltIns": "entry",
+                                },
+                            ]
+                        ]
+                    }
+                }
             }
         ]
     }
